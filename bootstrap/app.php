63cb14d1829b75e28bcac80317d6cf9ab13380d1
Ecio Silva <ecio.silva@creatix.com.br>
2026-02-20 03:58:28 -0300

    Força header 'accept: application/json, pois em alguns cenários de erro de validação, estava redirecionando e devolvendo html ao invés de devolver o erro http adequado e as mensagens correspondentes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Force JSON responses for all API routes
+        $middleware->api(prepend: \App\Http\Middleware\ForceJsonResponse::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         // Handle authentication exceptions for API routes
